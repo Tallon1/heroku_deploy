efd8837d87e02fcca617448ab81ceb8038122e47
Tallon1 <58697687+Tallon1@users.noreply.github.com>
2022-04-06 15:44:12 +0100

    Weather API Test

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,11 +1,11 @@
 class WeatherController < ApplicationController
   def _weather
-    require 'net/http'
-    require 'json'
+    # require 'net/http'
+    # require 'json'
 
-    @url = 'http://api.weatherapi.com/v1/current.json?key=78ea0243f8594dc8ad0134113220604&q=Dublin&aqi=no'
-    @uri = URI(@url)
-    @response = Net::HTTP.get(@uri)
-    @output = JSON.parse(@response)
+    # @url = 'http://api.weatherapi.com/v1/current.json?key=78ea0243f8594dc8ad0134113220604&q=Dublin&aqi=no'
+    # @uri = URI(@url)
+    # @response = Net::HTTP.get(@uri)
+    # @output = JSON.parse(@response)
   end
 end
