c54ebfd075cdfb423cbfd78aad30c7b11ef3ef2c
Tallon1 <58697687+Tallon1@users.noreply.github.com>
2022-04-06 19:44:46 +0100

    Update registrations_controller.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
+  def generate
+    require 'rubygems'
+    require 'random_password_generator'
+
+    @randoPassword = RandomPasswordGenerator.generate(10)
+    @output = @randoPassword
+  end
+
   # GET /resource/sign_up
   # def new
   #   super
