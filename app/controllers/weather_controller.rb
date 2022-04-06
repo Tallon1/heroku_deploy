a023058ef82f94baf075c26e84a2dcd2d9a58fbc
Tallon1 <58697687+Tallon1@users.noreply.github.com>
2022-04-06 14:10:32 +0100

    Update weather_controller.rb

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -4,9 +4,9 @@ class WeatherController < ApplicationController
     require 'net/http'
     require 'json'
 
-    @url = 'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid={018f010a6f5c869280fd4b4bc565bcd6}'
+    @url = 'https://api.openweathermap.org/data/2.5/weather?q={Dublin}&appid={018f010a6f5c869280fd4b4bc565bcd6}'
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
-    
+
   end
 end
