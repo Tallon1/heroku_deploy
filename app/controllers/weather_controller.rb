bbf5ee1331abe9df516dd112811884c694035fa8
Tallon1 <58697687+Tallon1@users.noreply.github.com>
2022-04-06 14:02:36 +0100

    API Test

---
+++ app/controllers/weather_controller.rb
@@ -0,0 +1,12 @@
+class WeatherController < ApplicationController
+  def _weather
+
+    require 'net/http'
+    require 'json'
+
+    @url = 'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid={018f010a6f5c869280fd4b4bc565bcd6}'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    
+  end
+end
