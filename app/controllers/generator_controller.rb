d8e0bdfd80a8c5b07a7a67eccc8e83de07cc9052
Tallon1 <58697687+Tallon1@users.noreply.github.com>
2022-04-06 19:31:25 +0100

    Gem Test

--- app/controllers/generator_controller.rb
+++ app/controllers/generator_controller.rb
@@ -1,4 +1,9 @@
 class GeneratorController < ApplicationController
   def generate
+    require 'rubygems'
+    require 'random_password_generator'
+
+    @randoPassword = RandomPasswordGenerator.generate(10)
+    @output = @randoPassword
   end
 end
