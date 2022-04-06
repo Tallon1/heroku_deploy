0bc0954e806017aa0eebf9dbc469b28418af7609
Tallon1 <58697687+Tallon1@users.noreply.github.com>
2022-04-06 11:43:37 +0100

    Clean up

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,30 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get users_show_url
+  setup do
+    @user = users(:one)
+  end
+  
+  test "try to find unknown room via url" do
+    get '/test'
     assert_response :success
   end
+
+  test "should get main rooms page" do
+    get '/rooms'
+    assert_response :success
 end
+
+# require "test_helper"
+
+# class InvalidRegistrationToken < ActionDispatch::IntegrationTest
+#   test "try to register new user without a legit login" do
+#     @user = users(:one)
+#     get '/users/sign_up'
+#     assert_response :success
+
+#     post users_url, params: {user: {email: @user.email}}
+#     assert_equal '/users', path
+#     assert_select 'li', "Password can't' be blank"
+#   end
+# end
