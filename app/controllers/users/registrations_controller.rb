17a07d64a0f8b2dd236e07d370b3c57111f51cfc
Tallon1 <58697687+Tallon1@users.noreply.github.com>
2022-04-06 19:07:01 +0100

    Gem Tests

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,10 +4,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
-  def
-    @output = RandomPasswordGenerator.generate(10)
-  end
-
   # GET /resource/sign_up
   # def new
   #   super
