c84d41cbc263e5584d5bd3e7681969eb4d8e0061
Tallon1 <58697687+Tallon1@users.noreply.github.com>
2022-04-07 18:43:54 +0100

    Gem Fix

--- app/controllers/generator_controller.rb
+++ app/controllers/generator_controller.rb
@@ -2,7 +2,7 @@ class GeneratorController < ApplicationController
   # Calling the password generator gem
   def generate
     require 'rubygems'
-    require 'random_password_generator'
+    require 'erratic_password_generator'
     
     @randoPassword = RandomPasswordGenerator.generate(24)
     @output = @randoPassword
